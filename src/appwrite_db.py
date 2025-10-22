a4c69f60496c77c5d99e967b2d3874cb840baedc
Ch Dawood Ayaz <162139620+Dawoodayaz20@users.noreply.github.com>
2025-10-22 23:50:22 +0500

    Refactor Appwrite client initialization with dotenv
    Load environment variables for Appwrite API key and project ID.

--- src/appwrite_db.py
+++ src/appwrite_db.py
@@ -1,13 +1,17 @@
 from appwrite.query import Query
 from appwrite.client import Client
 from appwrite.services.databases import Databases
+from dotenv import load_dotenv, find_dotenv 
 import os
 
+load_dotenv(find_dotenv())
+
 APPWRITE_API_KEY = os.getenv("APPWRITE_API_KEY")
+APPWRITE_PROJECT_ID = os.getenv("APPWRITE_PROJECT_ID")
 
 client = Client()
 client.set_endpoint("https://fra.cloud.appwrite.io/v1")  # or your self-hosted endpoint
-client.set_project("684d8fd1002b5462c7ab")
-client.set_key("APPWRITE_API_KEY")
+client.set_project(APPWRITE_PROJECT_ID)
+client.set_key(APPWRITE_API_KEY)
 
 db = Databases(client)
